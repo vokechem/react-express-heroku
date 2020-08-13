c5bc75f8e067b0a179eff3ceb74121ac15944b0c
Kelvin chemey <Kserem20@gmail.com>
2020-08-13 11:13:23 +0300

    first commit

---
+++ build/precache-manifest.23add289fd708317f0f451ee4bf5a485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe13f89230746d8fffcae273738d43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fbf194c94597de8840f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f528340de77abaf92b18",
+    "url": "/static/js/2.98914163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98914163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fbf194c94597de8840f",
+    "url": "/static/js/main.385250bb.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
